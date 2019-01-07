0c71ced2ab5d5da806ee8ab382594012df50f2dc
Seth Brady <sethgabrielbrady@gmail.com>
2019-01-07 14:03:59 -0500

    added reviews with one to many association

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,6 +15,8 @@ class Movie < ApplicationRecord
 
   validates :rating, inclusion: { in: RATINGS }
 
+  has_many :reviews, dependent: :destroy
+
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
   end
